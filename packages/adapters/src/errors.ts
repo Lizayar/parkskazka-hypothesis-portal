9061721f90ea73167473889ae0878f9c13a720fb
Lizayar <cristopherkarter@mail.ru>
2026-08-12 15:31:41 +0300

    feat: add read only adapters and snapshot pipeline

---
+++ packages/adapters/src/errors.ts
@@ -0,0 +1,18 @@
+export type AdapterErrorCode =
+  | "UNSUPPORTED_CAPABILITY"
+  | "INVALID_PERIOD"
+  | "INVALID_TIMEZONE"
+  | "INVALID_SCHEMA"
+  | "SOURCE_FAILURE";
+
+export class AdapterError extends Error {
+  constructor(
+    readonly code: AdapterErrorCode,
+    message: string,
+    readonly retryable = false,
+  ) {
+    super(message);
+    this.name = "AdapterError";
+  }
+}
+
